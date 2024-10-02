08e48edee596f58e17c98a7704c9bb2bec33917b
RyotaMiyahara5164 <b1022252@fun.ac.jp>
2024-10-02 15:55:55 +0900

    ios_android

---
+++ odajima/Assets/Scripts/Raw_Image.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raw_Image : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        //float width = Screen.width*11;
+        //float height = Screen.height*9;
+        
+        //rectTransform.sizeDelta = new Vector2(width, height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
